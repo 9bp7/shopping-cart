b62e82c2b249d67cc129035d8a1c781cebe5f867
Billy (Mac) <billyplant@hotmail.co.uk>
2021-01-13 11:41:06 +0000

    Updated styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 function Home() {
   return (
     <div className="wrap home">
-      <h1>We stock the highest quality Formula 1 Motor Car Replicas for the most devoted of racing fans.</h1>
+      <h1>The <span className="font-2">highest-quality</span> Formula 1 Motor Car Replicas for the <span className="font-2">most devoted</span> of racing fans.</h1>
       <Link to="/shop"><button>Shop the 2020 Collection &raquo;</button></Link>
+      <img src="/img/ferrari-trsp.png" alt="Ferrari F1 Car" />
     </div>
   );
 }
