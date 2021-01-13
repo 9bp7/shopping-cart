08ad77d916d63115b3a18b9effee8fb87335f8ed
Billy (Mac) <billyplant@hotmail.co.uk>
2021-01-13 13:58:38 +0000

    Change hero text

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Home() {
   return (
     <div className="wrap home">
-      <h1>The <span className="font-2">highest-quality</span> Formula 1 Motor Car Replicas for the <span className="font-2">most devoted</span> of racing fans.</h1>
+      <h1>The <span className="font-2">highest-quality, full-scale</span> Formula 1 Motor Car Replicas for the <span className="font-2">most devoted</span> of racing fans.</h1>
       <Link to="/shop"><button>Shop the 2020 Collection &raquo;</button></Link>
       <img src={process.env.PUBLIC_URL + "/img/ferrari-trsp.png"} alt="Ferrari F1 Car" />
     </div>
