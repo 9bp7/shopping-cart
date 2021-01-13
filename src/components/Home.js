2bb2350d61bd92f8d2ccc6a9ed01d1119e28dce2
Billy (Mac) <billyplant@hotmail.co.uk>
2021-01-13 12:33:36 +0000

    Update homepage to add publicurl

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div className="wrap home">
       <h1>The <span className="font-2">highest-quality</span> Formula 1 Motor Car Replicas for the <span className="font-2">most devoted</span> of racing fans.</h1>
       <Link to="/shop"><button>Shop the 2020 Collection &raquo;</button></Link>
-      <img src="/img/ferrari-trsp.png" alt="Ferrari F1 Car" />
+      <img src={process.env.PUBLIC_URL + "/img/ferrari-trsp.png"} alt="Ferrari F1 Car" />
     </div>
   );
 }
